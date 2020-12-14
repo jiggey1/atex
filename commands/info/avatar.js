66f3157e051db125c789dd14d50a55dad7ff21d7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 23:46:08 +0000

    Atex Update

---
+++ commands/info/avatar.js
@@ -0,0 +1,24 @@
+const {Discord, MessageEmbed} = require("discord.js");
+
+module.exports = {
+  name: "avatar",
+  category: "info",
+  description: "avatar Command",
+  run: async (client, message, args) => {
+
+    if (message.deletable) {
+        message.delete();
+    }
+
+    let msg = await message.channel.send("Grabbing avatar from the depths of Discord...");
+
+    let mentionedUser = message.mentions.users.first() || message.author;
+
+        const embed = new MessageEmbed()
+        .setImage(mentionedUser.displayAvatarURL())
+        .setColor("00ff00")
+        .setTitle("Gotcha! Here's The Avatar, Good Sir.")
+        .setFooter("Searched by " + message.author.tag)
+
+        message.channel.send(embed)
+}}
--- commands/info/help.js
+++ commands/info/help.js
@@ -43,7 +43,8 @@ module.exports = {
             }),
             reactions: {
                 '⏹': 'delete',
-                '▶': 'next'
+                '▶': 'next',
+                '⏩': 'last'
             }
         },
         {
